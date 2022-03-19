1c7eb9fbf471aecfb24c5556cd9dbca339796f2f
hek123 <hektor.ltb@gmail.com>
2022-03-19 13:51:57 +0100

    added buzzer on/off

--- holy_hack_arduino_code/holy_hack_arduino_code.ino
+++ holy_hack_arduino_code/holy_hack_arduino_code.ino
@@ -8,12 +8,18 @@ const int greenLEDpin = 4;
 const int blueLEDpin = 5;
 const int buzzer_pin = 10;
 
-// definitie variabelen
-String message;
+const char idle = 'a';
+const char find_bike = '1';
+const char stop_find = '0';
+const char buzz_on = '2';
+const char buzz_off = '3';
+
+char msg;
+bool buzz = true;
 
 void setup() {
 
-  // Monitor and bluetooth
+  // Bluetooth
   bluetooth.begin(9600);
   delay(100);
   Serial.begin(9600);
@@ -24,58 +30,52 @@ void setup() {
   pinMode(greenLEDpin, OUTPUT);
   pinMode(blueLEDpin, OUTPUT);
 
-  // Test connection
-  Serial.println("test");
-
-  // set varialbles
-  message = "";
+  // Define variables
+  msg = stop_find;
 }
 
 void loop() {
   // rcv message
   if (bluetooth.available()) {
-    message = bluetooth.readString();
-    bluetooth.println("Android Command: " + message);
+    msg = bluetooth.read();
+    bluetooth.println("Android Command: " + msg);
+    Serial.println(msg);
   }
 
-  // Control LED on Arduino board
-  if (message == "<turn on>"){
-    set_color(HIGH, HIGH, HIGH); // Turn on LED
-    tone(buzzer_pin, 1000);
-    bluetooth.println("LED is turned on\n"); // Send status message to Android
-    message = ""; // reset command
-  } else {
-    if (message == "<turn off>"){
+  // Control Arduino board
+  switch (msg){
+    case find_bike:
+      set_color(HIGH, HIGH, HIGH); // Turn on LED
+      if (buzz) tone(buzzer_pin, 1000);
+      bluetooth.println("LED is turned on\n"); // Send status message to Android
+      break;
+    case stop_find:
       set_color(LOW, LOW, LOW); // Turn off LED
       noTone(buzzer_pin);
       bluetooth.println("LED is turned off\n"); // Send status message to Android
-      message = ""; // reset command
-    }
+      break;
+    case buzz_on:
+      buzz = true;
+      bluetooth.println("Buzzer on");
+      break;
+    case buzz_off:
+      buzz = false;
+      bluetooth.println("Buzzer off");
+      break;
   }
+  msg = idle;
 }
 
 //-------EIGEN FUNCTIES-------
 
-void wait_for(String message){
-  bool klaar = true;
-  while (klaar){                 // wachten tot 'OK' wordt ontvangen
-    if (Serial.available()) {
-      String read = Serial.readString();
-      if (read == message+"\n") {
-        klaar = false;
-      }
-    }
-  }
-}
-
 void flikker() {
   for (int i=0; i<3; i++){
     set_color(HIGH, LOW, LOW);
-    delay(500);
+    delay(100);
     set_color(LOW, HIGH, LOW);
-    delay(500);
+    delay(100);
     set_color(LOW, LOW, HIGH);
-    delay(500);
+    delay(100);
   }
   set_color(LOW, LOW, LOW);
 }
@@ -84,15 +84,4 @@ void set_color(int r, int b, int g) {
   digitalWrite(redLEDpin, r);
   digitalWrite(blueLEDpin, b);
   digitalWrite(greenLEDpin, g);
-}
-
-String ontvang_BT_signaal() {
-  String boodschap = "";
-  while (bluetooth.available())
-  {
-    char c = bluetooth.read();
-    boodschap += c;
-    delay(10);
-  }
-  return boodschap;
 }
